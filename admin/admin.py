9eaba52ec5f1f7faed12c88615412d0bf5560e3b
AltairEzio <unicolour35@gmail.com>
2021-09-07 15:36:37 +0900

    Adding Kivy Examples

--- admin/admin.py
+++ admin/admin.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 class AdminWindow(BoxLayout):
     def __init__(self, **kwargs):
